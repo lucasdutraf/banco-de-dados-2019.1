6a05e940ec0c63691e2a88a9c2308533cb4d051a
Lucas Dutra <ldutra98@gmail.com>
2019-03-18 01:19:25 -0300

    Add user interface and capture input values

--- ativ-praticas/atividade-pratica1/aula2exer1_LucasDutra_17-0050939.py
+++ ativ-praticas/atividade-pratica1/aula2exer1_LucasDutra_17-0050939.py
@@ -18,15 +18,36 @@ class Vehicle:
         self._owner['cpf'] = cpf
 
 
+print('Bem vindo ao sistema de gerenciamento de carros!!')
+
 
 array = []
 
-while input('digite 0 ou 1') != '0':
-    vehicle = Vehicle('123123', 'Monza', 'jhd-1223')
-    vehicle.addOwner('aderbal', '09230192309')
-    array.append(vehicle)
 
-print(array[2].renavam)
+user_input = input("""
+(1) Inserir um novo veículo ao sistema.
+(2) Listar todos os carros inseridos.
+(0) Sair do programa.\n 
+Digite uma das opções listadas: """)
+while user_input != '0':
+
+    if user_input == '1':
+        user_renavam = input('Digite o renavan do veículo: ')
+        user_model = input('Digite o modelo do veículo: ')
+        user_licenseplate = input('Digite a placa do veículo: ')
+        vehicle = Vehicle(user_renavam, user_model, user_licenseplate)
+        user_name = input('Digite o nome do(a) dono(a) do veículo: ')
+        user_cpf = input('Digite o CPF do(a) dono(a) do veículo: ')
+        vehicle.addOwner(user_name, user_cpf)
+        array.append(vehicle)
+
+    user_input = input("""
+(1) Inserir um novo veículo ao sistema.
+(2) Listar todos os carros inseridos.
+(0) Sair do programa.\n 
+Digite uma das opções listadas: """)
+
+print(array[0])
 
 
 
