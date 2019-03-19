3fdd7c5a68249a873ee6ccb0f73076c079369834
Lucas Dutra <ldutra98@gmail.com>
2019-03-18 23:53:23 -0300

    Add print all vehicles logic

--- ativ-praticas/atividade-pratica1/aula2exer1_LucasDutra_17-0050939.py
+++ ativ-praticas/atividade-pratica1/aula2exer1_LucasDutra_17-0050939.py
@@ -1,4 +1,5 @@
 import os
+#falta filtrar saída, para não haver repetições e tudo sair ordenado
 
 class Person:
     def __init__(self, name, cpf):
@@ -32,8 +33,8 @@ Digite uma das opções listadas: """)
 
 fileOpened = open('banco.txt', 'a+')
 times_repeated = 0
-if os.stat('banco.txt').st_size == 0:
-    fileOpened.write('RENAVAM   MODELO   PLACA   CPF DO DONO(A)\n')
+# if os.stat('banco.txt').st_size == 0:
+#     fileOpened.write('RENAVAM   MODELO   PLACA   CPF DO DONO(A)\n')
     
 while user_input != '0':
 
@@ -46,11 +47,35 @@ while user_input != '0':
         user_cpf = input('Digite o CPF do(a) dono(a) do veículo: ')
         vehicle.addOwner(user_name, user_cpf)
         array.append(vehicle)
-        fileOpened.write('{} {} {} {}\n'.format(array[times_repeated].renavam, array[times_repeated].model, array[times_repeated].licensePlate, array[times_repeated]._owner['cpf']))
+        #fileOpened.write('{} {} {} {}\n'.format(array[times_repeated].renavam, array[times_repeated].model, array[times_repeated].licensePlate, array[times_repeated]._owner['cpf']))
 
     elif user_input == '2':
-        print(array[0])
+        read_fileOpened = open('banco.txt', 'r+')
+        if len(array) == 0:
+            display_array = []
+        else:
+            display_array = array    
+        if os.stat('banco.txt').st_size != 0:
+            aux_array = read_fileOpened.readlines()
+            aux_array_range = len(aux_array)
+            for line in range(aux_array_range):
+                vehicles_values = aux_array[line].split(' ')
+                print(len(vehicles_values))
+                aux_vehicle = Vehicle(vehicles_values[0], vehicles_values[1], vehicles_values[2])
+                aux_vehicle.addOwner(vehicles_values[4], vehicles_values[3])
+                display_array.append(aux_vehicle)
+        
+        display_array.sort(key=lambda x: x._owner['name'])
+        print('\n\n\nRENAVAM   MODELO   PLACA   CPF DO DONO(A) DONO(A)\n')
+        tamanho = len(display_array) 
 
+        for i in range(tamanho):
+            print('{} {} {} {} {}\n'.format(display_array[i].renavam, display_array[i].model, display_array[i].licensePlate, display_array[i]._owner['cpf'], display_array[i]._owner['name']))
+        print('\n\n\n')
+
+        read_fileOpened.close()
+
+        
     else:
         print('Por favor, digite uma opção válida.')
 
@@ -64,6 +89,13 @@ while user_input != '0':
 \n 
 Digite uma das opções listadas: """)
 
+array.sort(key=lambda x: x._owner['name'])
+
+tamanho = len(array) 
+
+for i in range(tamanho):
+    fileOpened.write('{} {} {} {} {}\n'.format(array[i].renavam, array[i].model, array[i].licensePlate, array[i]._owner['cpf'], array[i]._owner['name']))
+
 
 fileOpened.close()
 #print('{} {}'.format(array[0].model, array[0].renavam))
@@ -71,3 +103,7 @@ fileOpened.close()
 # f.write(array[0].renavam)
 # f.close()
 # print(vehicle._owner['cpf'])''
+
+#salvar no arquivo no final do programa para ter o sort antes
+
+#ordenar a cada iteração do while
